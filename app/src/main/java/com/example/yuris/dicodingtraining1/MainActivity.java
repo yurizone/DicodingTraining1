406298f9d7ea18d2b5ea53980a1332dce8ed9541
yurizone <yurizone.yuris@gmail.com>
2016-10-21 18:32:21 +0700

    Modul 1 : Activity

--- app/src/main/java/com/example/yuris/dicodingtraining1/MainActivity.java
+++ app/src/main/java/com/example/yuris/dicodingtraining1/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.yuris.dicodingtraining1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-//tes
+    private EditText edtPanjang, edtLebar;
+    private Button btnHitung;
+    private TextView txtLuas;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Hitung Luas Persegi Panjang");
+
+        edtPanjang = (EditText)findViewById(R.id.edt_panjang);
+        edtLebar = (EditText)findViewById(R.id.edt_lebar);
+        btnHitung = (Button)findViewById(R.id.btn_hitung);
+        txtLuas = (TextView)findViewById(R.id.txt_luas);
+
+        btnHitung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String panjang = edtPanjang.getText().toString().trim();
+                String lebar = edtLebar.getText().toString().trim();
+
+                double p = Double.parseDouble(panjang);
+                double l = Double.parseDouble(lebar);
+
+                double luas = p * l;
+
+                txtLuas.setText("Luas : "+luas);
+            }
+        });
     }
 }
